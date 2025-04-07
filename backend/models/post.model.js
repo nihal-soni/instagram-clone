cc5b86048b22da20ff46bfe9076812472c950251
nihal-soni <nihalsoni0523@gmail.com>
2025-04-08 01:43:23 +0530

    feat: add comment, conversation, and message models; update post model and DB config

--- backend/models/post.model.js
+++ backend/models/post.model.js
@@ -1,7 +1,12 @@
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
+import { User } from "./user.model";
 
 const postSchema = new mongoose.Schema({
     caption: {type: String, default: ''},
     image:{type: String, required: true},
-    autthor: {type:mongoose.Schema.Types.ObjectId, ref: 'User'}
+    autthor: {type:mongoose.Schema.Types.ObjectId, ref: 'User'},
+    likes:[{type:mongoose.Schema.Types.ObjectId, ref: 'User'}],
+    comments:[{type:mongoose.Schema.Types.ObjectId, ref: 'Comment'}]
 })
+
+export const Post = mongoose.model('Post', postSchema)
